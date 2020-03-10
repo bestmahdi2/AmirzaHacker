f949208ca4c7b7b976e7871e8962891c92010fbb
master <alibad979@gmail.com>
2020-03-09 20:21:45 -0700

    add comments and make some functions more readable

--- Compiler.py
+++ Compiler.py
@@ -7,67 +7,71 @@ QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 # use Hidpi icons
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
-class mains(Ui_MainWindow):
+class MainClass(Ui_MainWindow):         # inheritate from Ui_MainWindow
     def setupUi(self, MainWindow):
-        super().setupUi(MainWindow)
-        self.cancel = "false"
-        self.num = 0
+        super().setupUi(MainWindow)         # run the original codes in setupUi
+        self.cancel = "false"           # for clicking on the cancel button
+        self.num = 0 #
 
     def retranslateUi(self, MainWindow):
         super().retranslateUi(MainWindow)
         _translate = QtCore.QCoreApplication.translate
-        self.pushButton.setShortcut(_translate("MainWindow", "Return"))
+        self.pushButton.setShortcut(_translate("MainWindow", "Return"))             # make shortcut for pushbutton
 
-    def checker(self,num):
+    # add checkbox numbers to self.num
+    def number_adder_func(self,num):
         self.num += num
-    def firstpush(self):
+
+    def checkbox_checker_func(self):
         self.num = 0
-        check3 = self.checkBox3.isChecked()
+        check3 = self.checkBox3.isChecked()                 # if checkbox3 is checked check3 is True else is False
         check4 = self.checkBox4.isChecked()
         check5 = self.checkBoxMore.isChecked()
 
         if check3 ==True:
-            self.checker(3)
+            self.number_adder_func(3)
         if check4 ==True:
-            self.checker(4)
+            self.number_adder_func(4)
         if check5 ==True:
-            self.checker(5)
-        if check3 == False and check4 == False and check5 == False :
+            self.number_adder_func(5)
+
+        if check3 == False and check4 == False and check5 == False:             # show error if none of checkboxes checked
             self.label_Error.setStyleSheet("color:red")
             self.label_Error.setText("Check at least one checkbox")
-        if check3 == True or check4 == True or check5 == True:
-            self.entrance()
 
-    def spell(self):
+        if check3 == True or check4 == True or check5 == True:              # go to next function if at least one of checkboxes is  checked
+            self.empty_entrance_func()
+
+    def language_checker_func(self):
         persian = ["ا", "آ", "ب", "پ", "ت", "ث", "ج", "چ", "ح", "خ", "د", "ذ", "ر", "ز", "ژ", "س", "ش", "ص", "ض", "ط",
                    "ظ", "ع", "غ", "ف", "ق", "ک", "گ", "ل", "ن", "و", "ه", "ی", "م", " "]
-        text = self.lineEdit.text()
-        textlist = text.split()
+        text = self.lineEdit.text()         # get the text in the lineEdit
+        textlist = text.split()         # split them and make a list
         for i in textlist:
             if i not in persian:
                 self.label_Error.setStyleSheet("color:red")
                 self.label_Error.setText("Incorrect entrance")
-                self.pushButton.setEnabled(False)
+                self.pushButton.setEnabled(False)           # disable the pushbutton
             else:
                 ui.label_Error.setText("")
                 ui.pushButton.setEnabled(True)
-        # for i in te
 
-    def entrance(self):
+    def empty_entrance_func(self):
         text = self.lineEdit.text()
         text = text.replace("  "," ").replace("   "," ").replace("    "," ")
         if text == "" or text ==" ":
             self.label_Error.setText("Empty entrance")
         else:
             self.pushB_clicked()
+
     def pushB_clicked(self):
         self.cancel = "false"
         self.wrong = False
         self.textBrowser.setText("")
 
-        m = AmirzaClass()
+        m = AmirzaClass()           # make object from AmirzaClass
         text = self.lineEdit.text()
-        m.amirza(text,self.num)
+        m.amirzafunc(text,self.num)             # send the self.num (number of checkboxes) to amirzafunc
 
         self.label_Error.setStyleSheet("color:white")
         self.label_Error.setText("Processing...")
@@ -76,30 +80,25 @@ class mains(Ui_MainWindow):
         m.select_all_tasks(conn)
 
         answers = []
-                # print()
-        self.progressBar.setMaximum(len(m.ultimListO))
+        self.progressBar.setMaximum(len(m.ultimList_undup))             # set maximum for progressbar by length of the ultimList_undup
 
-            # print(m.ultimList)
-        def cancels():
+        def cancel_func():          # when the cancel button clicked
                 ui.cancel = "True"
                 ui.progressBar.setProperty("value", 0)
 
-        self.pushButton_2.clicked.connect(cancels)
+        self.pushButton_2.clicked.connect(cancel_func)          # connect cancel button to cancel_func
 
-        for i in m.ultimListO:
-                self.progressBar.setProperty("value", m.ultimListO.index(i) + 1)
+        for i in m.ultimList_undup:
+                self.progressBar.setProperty("value", m.ultimList_undup.index(i) + 1)              # set value of the progressbar by index of (i) in m.ultimList_undup
                 if i in m.diclist:
-                    answers.append(i)
+                    answers.append(i)           # append answers if the word is in the database
                 QtWidgets.QApplication.processEvents()
                 if ui.cancel == "True":
-                    break
-                # self.progressBar.setValue(progress)
-                # QtWidgets.QApplication.processEvents()
+                    break           # break if cancel button clicked
         try:
-            nondup = list(dict.fromkeys(answers))
-            # print(answers)
-            self.to_show = " ".join(nondup)
-            self.textBrowser.setText(self.to_show)
+            nondup = list(dict.fromkeys(answers))       # remove duplicated words in answers
+            self.to_show = " ".join(nondup)         # join the nondup objects with space character
+            self.textBrowser.setText(self.to_show)          # set textBrowser to show the to_show
 
         except :
             self.label_Error.setText("Incorrect entrance")
@@ -114,11 +113,11 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = mains()
+    ui = MainClass()
     ui.setupUi(MainWindow)
 
-    ui.lineEdit.textEdited.connect(ui.spell)
-    ui.pushButton.clicked.connect(ui.firstpush)
+    ui.lineEdit.textEdited.connect(ui.language_checker_func)            # check language input while typing
+    ui.pushButton.clicked.connect(ui.checkbox_checker_func)             # connect pushbutton
 
 
     MainWindow.show()
--- ProgramFile/Amirza.py
+++ ProgramFile/Amirza.py
@@ -1,23 +1,26 @@
 from itertools import permutations
 import sqlite3
 
-class AmirzaClass:
-    def amirza(self , text, number):
-        # text = input("enter: ")
-        textlist = text.replace("-"," ").replace("  "," ").split(" ")
-            # textlist1 = text.replace("-"," ").repalce("  "," ").split(" ")
-        x = 3
-        i = 0
-        lister = ["default", "default", "default", "default", "default","default","default","default""default" ]
 
+class AmirzaClass:
+    def amirzafunc(self, text, number):             # number is the value of checkboxes which user check
+        textlist = text.replace("-", " ").replace("  ", " ").split(" ")             # change string to list
+        x = 3           # we want words with more than 3 characters
+        i = 0           # counter
+
+        # answer keeper:
+        lister = ["default", "default", "default", "default", "default", "default", "default", "default""default"]
+
+        # 4, 3, 5 are checked
         if number == 12:
+            # it make all possible words with letters that entered ,
+            # from 3 letters in a word till the length of the string that entered in a word
             while x <= len(textlist):
                 lister[i] = (list(permutations(textlist, x)))
                 i += 1
                 x += 1
-        #     # print(lister)
 
-        if number == 3 :
+        if number == 3:
             while x <= number:
                 lister[i] = (list(permutations(textlist, x)))
                 x += 1
@@ -34,14 +37,16 @@ class AmirzaClass:
                 x += 1
                 i += 1
 
+        # 3 and 4 checked
         if number == 7:
-            while 3 <= x <= 4 :
+            while 3 <= x <= 4:
                 lister[i] = (list(permutations(textlist, x)))
                 x += 1
                 i += 1
 
+        # 3 and 5 checked but 4 not checked
         if number == 8:
-            while 3 <= x < 4 :
+            while 3 <= x < 4:
                 lister[i] = (list(permutations(textlist, x)))
                 x += 1
                 i += 1
@@ -51,59 +56,68 @@ class AmirzaClass:
                 x += 1
                 i += 1
 
-
         if number == 9:
             x = 4
-            while 4 <= x < 5 :
+            while 4 <= x < 5:
                 lister[i] = (list(permutations(textlist, x)))
                 x += 1
                 i += 1
-            # x = 5
             while 5 <= x <= len(textlist):
                 lister[i] = (list(permutations(textlist, x)))
                 x += 1
                 i += 1
 
         lister_new = []
-        for i in lister :
+
+        # make a new list for answers
+        for i in lister:
             if i != "default" and i != "defaultdefault":
                 lister_new.append(i)
-        # print(lister_new)
 
         self.ultimList = []
+        # join the letters to make words
         for i in lister_new:
-            for k in i :
+            for k in i:
                 self.ultimList.append("".join(k))
-        # print(self.ultimList)
 
-        self.ultimListO = list(dict.fromkeys(self.ultimList))
-        # print(self.ultimListO)
+        # remove all duplicated words
+        self.ultimList_undup = list(dict.fromkeys(self.ultimList))
 
+    # creat connection with the sqlite database
     def create_connection(self, db_file):
         conn = None
         conn = sqlite3.connect(db_file)
         return conn
 
-    def select_all_tasks(self,conn):
+    # select all tasks in a column
+    def select_all_tasks(self, conn):
         cur = conn.cursor()
         cur.execute("SELECT entry from mdx")
         rows = cur.fetchall()
+
+        # save all data in a column to use in future
         self.diclist = []
         for row in rows:
             self.diclist.append(row[0])
-        # print(lister)
-#
+
+#####################################
+# this is for test , just uncomment  below :
+
 # cla = AmirzaClass()
-# cla.amirza("گ ب  د آ ب ا د",5)
-#
+# cla.amirzafunc("گ ب  د آ ب ا د",4)
+# # Moin.db is the name of the database
 # conn = cla.create_connection(r"Moin.db")
 # cla.select_all_tasks(conn)
 #
 # print(cla.ultimList)
+# print(cla.ultimList_undup)
+#
 # answers = []
-# for i in cla.ultimList :
+# for i in cla.ultimList_undup :
 #     if i in cla.diclist :
 #         answers.append(i)
 #
 # print(answers)
-# print(cla.ultimListO)
+
+
+#####################################
